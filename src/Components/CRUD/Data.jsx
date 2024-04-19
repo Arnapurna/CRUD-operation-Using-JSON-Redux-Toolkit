080c504edead3d47eaf0aca162923785fece7595
Arnapurna <bediarnapurna20@gmail.com>
2024-04-19 12:02:38 +0530

    Manual data file added

---
+++ src/Components/CRUD/Data.jsx
@@ -0,0 +1,26 @@
+export const userList=[
+    {
+        id:1,
+        name:"abc",
+        email:"abc@gmail.com",
+        
+    },
+    {
+        id:2,
+        name:"def",
+        email:"def@gmail.com",
+        
+    },
+    {
+        id:3,
+        name:"ghi",
+        email:"ghi@gmail.com",
+        
+    },
+    {
+        id:4,
+        name:"xyz",
+        email:"xyz@gmail.com",
+        
+    },
+]
